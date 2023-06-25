5ea81c9b766e4068b2d10a52bdf7a04088cf26bd
Mesut Yılmaz <mesut.yilmaz@stu.fsm.edu.tr>
2023-06-26 00:42:26 +0300

    BaseRepository unused functions removed

--- app/Http/Repositories/BaseEloquentRepository.php
+++ app/Http/Repositories/BaseEloquentRepository.php
@@ -3,8 +3,6 @@
 namespace App\Http\Repositories;
 
 use App\Http\RepositoryContracts\IBaseEloquentRepository;
-use App\Repositories\BaseRepository\Traits\CacheResults;
-use App\Repositories\BaseRepository\Traits\ThrowsHttpExceptions;
 use Closure;
 use Exception;
 use Illuminate\Contracts\Container\BindingResolutionException;
@@ -26,8 +24,6 @@ class BaseEloquentRepository implements IBaseEloquentRepository
      */
     protected $model;
 
-    protected $originalModel;
-
     /**
      * Array of method names of relationships available to use
      * @var array
@@ -40,23 +36,13 @@ class BaseEloquentRepository implements IBaseEloquentRepository
      */
     protected array $requiredRelationships = [];
 
-    /**
-     * Array of traits being used by the repository.
-     * @var array
-     */
-    protected array $uses = [];
+    private string $orderBy = 'id';
 
-    protected int $cacheTtl = 60;
+    private string $orderType = 'desc';
 
-    protected bool $caching = true;
+    private bool $withPagination = false;
 
-    private string $orderBy;
-
-    private string $orderType;
-
-    private bool $withPagination;
-
-    private int $perPage;
+    private int $perPage = 30;
 
 
     /**
@@ -65,11 +51,16 @@ class BaseEloquentRepository implements IBaseEloquentRepository
      */
     public function __construct()
     {
-        $this->originalModel = $this->model;
-        $this->renewModel();
-        $this->setUses();
-        $this->setOrderDefaults();
-        $this->setPaginationDefaults();
+        $this->model = app()->make($this->model);
+    }
+
+    public function parseRequest(array $requestArray): void
+    {
+        $this->setOrder($requestArray);
+
+        $this->setTrashed($requestArray);
+
+        $this->setPagination($requestArray);
     }
 
     /**
@@ -77,89 +68,22 @@ class BaseEloquentRepository implements IBaseEloquentRepository
      *
      * @param string $columns specific columns to select
      * @param string $orderBy column to sort by
-     * @param string $sort sort direction
+     * @param string $orderType sort direction
      * @return Collection|LengthAwarePaginator
      */
-    public function getAll($columns = ['*'], $orderBy = null, $sort = null)
+    public function getAll($columns = ['*'], $orderBy = null, $orderType = null)
     {
         if ($this->withPagination) {
             return $this->getPaginated($this->perPage);
         }
 
-        if ($orderBy == null) {
-            $orderBy = $this->orderBy;
-        }
+        $orderBy = $orderBy ?? $this->orderBy;
+        $orderType = $orderType ?? $this->orderType;
 
-        if ($sort == null) {
-            $sort = $this->orderType;
-        }
-
-        $query = function () use ($columns, $orderBy, $sort) {
-
-            return $this->model
-                ->with($this->requiredRelationships)
-                ->orderBy($orderBy, $sort)
-                ->get($columns);
-        };
-
-        return $this->doQuery($query);
-    }
-
-    public function getQuery(): Builder
-    {
         return $this->model
-            ->with($this->requiredRelationships);
-    }
-
-
-    /**
-     * Get all items
-     *
-     * @param string[] $columns specific columns to select
-     * @param null $secondOrderBy column to sort by
-     * @param null $orderBy
-     * @param null $sort sort direction
-     * @return Collection|LengthAwarePaginator
-     */
-    public function getAllWithDoubleOrderBy($columns = ['*'], $secondOrderBy = null, $orderBy = null, $sort = null)
-    {
-        if ($this->withPagination) {
-            return $this->getPaginatedWithDoubleOrderBy($this->perPage, $secondOrderBy, $orderBy, $sort);
-        }
-
-        if ($secondOrderBy == null) {
-            $secondOrderBy = $this->orderBy;
-        }
-
-        if ($orderBy == null) {
-            $orderBy = $this->orderBy;
-        }
-
-        if ($sort == null) {
-            $sort = $this->orderType;
-        }
-
-        $query = function () use ($secondOrderBy, $columns, $orderBy, $sort) {
-
-            return $this->model
                 ->with($this->requiredRelationships)
-                ->orderBy($secondOrderBy, $sort)
-                ->orderBy($orderBy, $sort)
+                ->orderBy($orderBy, $orderType)
                 ->get($columns);
-        };
-
-        return $this->doQuery($query);
-    }
-
-    public function count()
-    {
-        $query = function () {
-
-            return $this->model
-                ->count();
-        };
-
-        return $this->doQuery($query);
     }
 
     /**
@@ -170,96 +94,22 @@ class BaseEloquentRepository implements IBaseEloquentRepository
      * @param string $sort Sort direction
      * @return Paginator
      */
-    public function getPaginated($paged = 15, $orderBy = null, $sort = null): LengthAwarePaginator
+    public function getPaginated($paged = 15, $orderBy = null, $orderType = null): LengthAwarePaginator
     {
-        if ($orderBy == null) {
-            $orderBy = $this->orderBy;
-        }
+        $orderBy = $orderBy ?? $this->orderBy;
+        $orderType = $orderType ?? $this->orderType;
 
-        if ($sort == null) {
-            $sort = $this->orderType;
-        }
-
-        $query = function () use ($paged, $orderBy, $sort) {
-
-            return $this->model
+        return $this->model
                 ->with($this->requiredRelationships)
-                ->orderBy($orderBy, $sort)
+                ->orderBy($orderBy, $orderType)
                 ->paginate($paged);
-        };
-
-        return $this->doQuery($query);
     }
 
-    /**
-     * Get paged items
-     *
-     * @param integer $paged Items per page
-     * @param string $secondOrderBy Column to sort by
-     * @param string $orderBy Column to sort by
-     * @param string $sort Sort direction
-     * @return LengthAwarePaginator
-     */
-    public function getPaginatedWithDoubleOrderBy(
-        $paged = 15,
-        $secondOrderBy = null,
-        $orderBy = null,
-        $sort = null
-    ): LengthAwarePaginator {
-        if ($orderBy == null) {
-            $orderBy = $this->orderBy;
-        }
-
-        if ($secondOrderBy == null) {
-            $secondOrderBy = $this->orderBy;
-        }
-
-        if ($sort == null) {
-            $sort = $this->orderType;
-        }
-
-        $query = function () use ($secondOrderBy, $paged, $orderBy, $sort) {
-
-            return $this->model
-                ->with($this->requiredRelationships)
-                ->orderBy($secondOrderBy, $sort)
-                ->orderBy($orderBy, $sort)
-                ->paginate($paged);
-        };
-
-        return $this->doQuery($query);
-    }
-
-    /**
-     * Items for select options
-     *
-     * @param string $data column to display in the option
-     * @param string $key column to be used as the value in option
-     * @param string $orderBy column to sort by
-     * @param string $sort sort direction
-     * @return array           array with key value pairs
-     */
-    public function getForSelect(string $data, $key = 'id', $orderBy = null, $sort = null): array
+    public function count()
     {
-        if ($orderBy == null) {
-            $orderBy = $this->orderBy;
-        }
-
-        if ($sort == null) {
-            $sort = $this->orderType;
-        }
-
-        $query = function () use ($data, $key, $orderBy, $sort) {
-            return $this->model
-                ->with($this->requiredRelationships)
-                ->orderBy($orderBy, $sort)
-                ->pluck($data, $key)
-                ->all();
-        };
-
-        return $this->doQuery($query);
+        return $this->model->count();
     }
-
+    
     /**
      * Get item by its id
      *
@@ -268,98 +118,9 @@ class BaseEloquentRepository implements IBaseEloquentRepository
      */
     public function getById(int $modelId): ?Model
     {
-        $query = function () use ($modelId) {
-            return $this->model
+        return $this->model
                 ->with($this->requiredRelationships)
                 ->find($modelId);
-        };
-
-        return $this->doQuery($query);
-    }
-
-    public function getManyById(array $modelIds)
-    {
-        $query = function () use ($modelIds) {
-            return $this->model
-                ->with($this->requiredRelationships)
-                ->findMany($modelIds);
-        };
-
-        return $this->doQuery($query);
-    }
-
-    public function getByUuid(string $modelUuid): ?Model
-    {
-        $query = function () use ($modelUuid) {
-            return $this->model
-                ->where('uuid', $modelUuid)
-                ->first();
-        };
-        return $this->doQuery($query);
-    }
-
-    /**
-     * @param $uuid
-     * @param array $with
-     * @return Builder|Model|object|null
-     */
-    public function findByUuidWith($uuid, array $with): ?Model
-    {
-        return $this->model->with($with)->where('uuid', $uuid)->first();
-    }
-
-    /**
-     * Get instance of model by column
-     *
-     * @param mixed $term search term
-     * @param string $column column to search
-     * @return Model
-     */
-    public function getItemByColumn($term, $column = 'slug'): Model
-    {
-        $query = function () use ($term, $column) {
-            return $this->model
-                ->with($this->requiredRelationships)
-                ->where($column, '=', $term)
-                ->first();
-        };
-
-        return $this->doQuery($query);
-    }
-
-    /**
-     * Get instance of model by column
-     *
-     * @param mixed $term search term
-     * @param string $column column to search
-     * @return Collection
-     */
-    public function getCollectionByColumn($term, $column = 'slug'): Collection
-    {
-        $query = function () use ($term, $column) {
-            return $this->model
-                ->with($this->requiredRelationships)
-                ->where($column, '=', $term)
-                ->get();
-        };
-
-        return $this->doQuery($query);
-    }
-
-    /**
-     * Get item by id or column
-     *
-     * @param mixed $term id or term
-     * @param string $column column to search
-     * @return Model
-     */
-    public function getActively($term, $column = 'slug'): Model
-    {
-        if (is_numeric($term)) {
-            return $this->getById($term);
-        }
-
-        return $this->getItemByColumn($term, $column);
     }
 
     /**
@@ -404,43 +165,6 @@ class BaseEloquentRepository implements IBaseEloquentRepository
     }
 
     /**
-     * Update a record using the primary key.
-     *
-     * @param $modelId
-     * @param $data array
-     * @return bool
-     */
-    public function updateMass($modelId, array $data): bool
-    {
-        return $this->model->where($this->model->getKeyName(), $modelId)->update($data);
-    }
-
-    /**
-     * Update or crate a record and return the entity
-     *
-     * @param array $attributes
-     * @param array $values
-     * @return mixed
-     */
-    public function updateOrCreate(array $attributes, array $values = []): ?Model
-    {
-        return $this->model->updateOrCreate($attributes, $values);
-    }
-
-    /**
-     * Update or crate a record and return the entity
-     *
-     * @param HasMany|HasOne $relation
-     * @param array $attributes
-     * @param array $values
-     * @return mixed
-     */
-    public function updateOrCreateForChild($relation, array $attributes, array $values = []): ?Model
-    {
-        return $relation->updateOrCreate($attributes, $values);
-    }
-
-    /**
      * @param array $attributes
      * @param array $values
      * @return Builder|Model|mixed
@@ -448,18 +172,6 @@ class BaseEloquentRepository implements IBaseEloquentRepository
     public function firstOrCreate(array $attributes, array $values = [])
     {
         return $this->model->firstOrCreate($attributes, $values);
-    }
-
-    /**
-     * Delete a record by the primary key.
-     *
-     * @param $modelId
-     * @return bool
-     * @throws Exception
-     */
-    public function deleteMass($modelId): bool
-    {
-        return $this->model->where($this->model->getKeyName(), $modelId)->delete();
     }
 
     /**
@@ -526,14 +238,7 @@ class BaseEloquentRepository implements IBaseEloquentRepository
         return $this;
     }
 
-    public function parseRequest(array $requestArray): void
-    {
-        $this->setOrder($requestArray);
-
-        $this->setTrashed($requestArray);
-
-        $this->setPagination($requestArray);
-    }
+    
 
     /**
      * Determines only to get soft deleted rows
@@ -551,120 +256,26 @@ class BaseEloquentRepository implements IBaseEloquentRepository
         return $this;
     }
 
-    /**
-     * Perform the repository query.
-     *
-     * @param $callback
-     * @return mixed
-     */
-    protected function doQuery($callback)
-    {
-        $previousMethod = debug_backtrace(null, 2)[1];
-        $methodName = $previousMethod['function'];
-        $arguments = $previousMethod['args'];
-
-        $result = $this->doBeforeQuery($callback, $methodName, $arguments);
-
-        return $this->doAfterQuery($result, $methodName, $arguments);
-    }
-
-    /**
-     *  Apply any modifiers to the query.
-     *
-     * @param $callback
-     * @param $methodName
-     * @param $arguments
-     * @return mixed
-     */
-    private function doBeforeQuery($callback, $methodName, $arguments)
-    {
-        $traits = $this->getUsedTraits();
-        
-        if (in_array(CacheResults::class, $traits) && $this->caching && $this->isCacheableMethod($methodName)) {
-            return $this->processCacheRequest($callback, $methodName, $arguments);
-        }
-
-        return $callback();
-    }
-
-    /**
-     * Handle the query result.
-     *
-     * @param $result
-     * @param $methodName
-     * @param $arguments
-     * @return mixed
-     */
-    private function doAfterQuery($result, $methodName, $arguments)
-    {
-        $traits = $this->getUsedTraits();
-
-        if (in_array(CacheResults::class, $traits)) {
-            // Reset caching to enabled in case it has just been disabled.
-            $this->caching = true;
-        }
-
-        if (in_array(ThrowsHttpExceptions::class, $traits)) {
-            if ($this->shouldThrowHttpException($result, $methodName)) {
-                $this->throwNotFoundHttpException($methodName, $arguments);
-            }
-
-            $this->exceptionsDisabled = false;
-        }
-
-        $this->renewModel();
-
-        return $result;
-    }
-
-    /**
-     * @return int
-     */
-    protected function getCacheTtl(): int
-    {
-        return $this->cacheTtl;
-    }
-
-    /**
-     * @return $this
-     */
-    protected function setUses(): BaseEloquentRepository
-    {
-        $this->uses = array_flip(class_uses_recursive(get_class($this)));
-
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
-    protected function getUsedTraits(): array
-    {
-        return $this->uses;
-    }
-
-    private function setOrderDefaults(): void
-    {
-        $this->orderBy = "id";
-        $this->orderType = "desc";
-    }
-
-    private function setPaginationDefaults(): void
-    {
-        $this->withPagination = false;
-        $this->perPage = 30;
-    }
 
     private function setOrder($requestArray): void
     {
-        $this->setOrderDefaults();
-
         if (isset($requestArray["order_by"])) {
             $this->orderBy = $requestArray["order_by"];
         }
 
         if (isset($requestArray["order_type"])) {
             $this->orderType = $requestArray["order_type"];
+        }
+    }
+
+    private function setPagination(array $requestArray)
+    {
+        if (isset($requestArray["with_pagination"]) && convertToBoolean($requestArray["with_pagination"])) {
+            $this->withPagination = true;
+        }
+
+        if (isset($requestArray["per_page"]) && is_numeric($requestArray["per_page"])) {
+            $this->perPage = (int)$requestArray["per_page"];
         }
     }
 
@@ -682,24 +293,6 @@ class BaseEloquentRepository implements IBaseEloquentRepository
         if (isset($requestedArray["only_trashed"])) {
             $this->model = $this->model->onlyTrashed();
         }
-    }
-
-    private function setPagination(array $requestArray)
-    {
-        $this->setPaginationDefaults();
-
-        if (isset($requestArray["with_pagination"]) && convertToBoolean($requestArray["with_pagination"])) {
-            $this->withPagination = true;
-        }
-
-        if (isset($requestArray["per_page"]) && is_numeric($requestArray["per_page"])) {
-            $this->perPage = (int)$requestArray["per_page"];
-        }
-    }
-
-    private function renewModel()
-    {
-        $this->model = app()->make($this->originalModel);
     }
 
     /**
--- app/Http/RepositoryContracts/IBaseEloquentRepository.php
+++ app/Http/RepositoryContracts/IBaseEloquentRepository.php
@@ -20,18 +20,7 @@ interface IBaseEloquentRepository
      */
     public function getAll($columns = null, $orderBy = null, $sort = null);
 
-    public function getQuery(): Builder;
-
-    /**
-     * Get all items
-     *
-     * @param string $columns specific columns to select
-     * @param string $secondOrderBy column to sort by
-     * @param string $orderBy second column to sort by
-     * @param string $sort sort direction
-     */
-    public function getAllWithDoubleOrderBy($columns = null, $secondOrderBy = null, $orderBy = null, $sort = null);
-
+    
     public function count();
 
     /**
@@ -44,73 +33,11 @@ interface IBaseEloquentRepository
     public function getPaginated($paged = 15, $orderBy = null, $sort = null);
 
     /**
-     * Get paged items
-     *
-     * @param integer $paged Items per page
-     * @param string $secondOrderBy Column to sort by
-     * @param string $orderBy Column to sort by
-     * @param string $sort Sort direction
-     */
-    public function getPaginatedWithDoubleOrderBy($paged = 15, $secondOrderBy = null, $orderBy = null, $sort = null);
-
-    /**
-     * Items for select options
-     *
-     * @param string $data column to display in the option
-     * @param string $key column to be used as the value in option
-     * @param string $orderBy column to sort by
-     * @param string $sort sort direction
-     * @return array  array with key value pairs
-     */
-    public function getForSelect(string $data, $key = 'id', $orderBy = null, $sort = null): array;
-
-    /**
      * Get item by its id
      *
      * @param int $modelId
      */
     public function getById(int $modelId);
-
-    /**
-     * @param array $modelIds
-     */
-    public function getManyById(array $modelIds);
-
-    /**
-     * @param string $modelUuid
-     */
-    public function getByUuid(string $modelUuid): ?Model;
-
-    /**
-     * @param string $uuid
-     * @param array $with
-     * @return Model|null
-     */
-    public function findByUuidWith(string $uuid, array $with): ?Model;
-
-    /**
-     * Get instance of model by column
-     *
-     * @param mixed $term search term
-     * @param string $column column to search
-     */
-    public function getItemByColumn($term, $column = 'slug');
-
-    /**
-     * Get instance of model by column
-     *
-     * @param mixed $term search term
-     * @param string $column column to search
-     */
-    public function getCollectionByColumn($term, $column = 'slug');
-
-    /**
-     * Get item by id or column
-     *
-     * @param mixed $term id or term
-     * @param string $column column to search
-     */
-    public function getActively($term, $column = 'slug');
 
     /**
      * Create new using mass assignment
@@ -137,36 +64,11 @@ interface IBaseEloquentRepository
     public function createChildren(HasMany $relation, array $data): Collection;
 
     /**
-     * Update or crate a record and return the entity
-     *
-     * @param array $attributes
-     * @param array $values
-     * @return Model|null
-     */
-    public function updateOrCreate(array $attributes, array $values = []): ?Model;
-
-    /**
-     * @param HasMany|HasOne $relation
-     * @param array $attributes
-     * @param array $values
-     * @return Model|null
-     */
-    public function updateOrCreateForChild($relation, array $attributes, array $values = []): ?Model;
-
-    /**
      * @param array $attributes
      * @param array $values
      * @return mixed
      */
     public function firstOrCreate(array $attributes, array $values = []);
-
-    /**
-     * Delete a record by it's ID.
-     *
-     * @param $modelId
-     * @return bool
-     */
-    public function deleteMass($modelId): bool;
 
     /**
      * Delete a record by its model
@@ -193,13 +95,6 @@ interface IBaseEloquentRepository
      * @return mixed
      */
     public function update(Model $model, array $data): ?Model;
-
-    /**
-     * @param $modelId
-     * @param array $data
-     * @return bool
-     */
-    public function updateMass($modelId, array $data): bool;
 
     /**
      * @return BaseEloquentRepository
