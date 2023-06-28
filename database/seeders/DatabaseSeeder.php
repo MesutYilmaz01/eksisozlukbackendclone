8e7b66c71bfa7fce9f652cd77c8072cd4ca5850e
Mesut Yılmaz <mesut.yilmaz@stu.fsm.edu.tr>
2023-06-28 20:57:49 +0300

    FollowSeeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             HeaderSeeder::class,
-            EntrySeeder::class
+            EntrySeeder::class,
+            FollowSeeder::class,
         ]);        
     }
 }
